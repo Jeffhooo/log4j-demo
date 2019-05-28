19f1c60291f24d570134b33993ae78eed99b4a01
He_Junfeng <he_j@worksap.co.jp>
2019-05-28 20:28:39 +0800

    improve
    Signed-off-by: He_Junfeng <he_j@worksap.co.jp>

--- src/main/java/jeff/demo/Log4jDemo.java
+++ src/main/java/jeff/demo/Log4jDemo.java
@@ -12,42 +12,49 @@ public class Log4jDemo {
     public static void main(String[] args) {
 
         new Thread(() -> {
-                Log4jUtil.addFileAppender("thread-0", logger);
-                MDC.put("logFileName", "thread-0");
+            // add appender
+            Log4jUtil.addFileAppender("thread-0", logger);
+            MDC.put("logFileName", "thread-0");
 
-                logger.trace("thread-0: Hello World!");
-                logger.debug("thread-0: How are you today?");
-                logger.info("thread-0: I am fine.");
-                logger.warn("thread-0: I love programming.");
-                logger.error("thread-0: I am programming.");
+            // start log
+            logger.trace("thread-0: Hello World!");
+            logger.debug("thread-0: How are you today?");
+            logger.info("thread-0: I am fine.");
+            logger.warn("thread-0: I love programming.");
+            logger.error("thread-0: I am programming.");
 
-                MDC.remove("logFileName");
-                Log4jUtil.stopFileAppender("thread-0", logger);
+            MDC.remove("logFileName");
+            Log4jUtil.stopFileAppender("thread-0", logger);
         }).start();
 
         new Thread(() -> {
-                Log4jUtil.addFileAppender("thread-1", logger);
-                MDC.put("logFileName", "thread-1");
+            // add appender
+            Log4jUtil.addFileAppender("thread-1", logger);
+            MDC.put("logFileName", "thread-1");
 
-                logger.trace("thread-1: Hello World!");
-                logger.debug("thread-1: How are you today?");
-                logger.info("thread-1: I am fine.");
-                logger.warn("thread-1: I love programming.");
-                logger.error("thread-1: I am programming.");
+            // start log
+            logger.trace("thread-1: Hello World!");
+            logger.debug("thread-1: How are you today?");
+            logger.info("thread-1: I am fine.");
+            logger.warn("thread-1: I love programming.");
+            logger.error("thread-1: I am programming.");
 
-                MDC.remove("logFileName");
-                Log4jUtil.stopFileAppender("thread-1", logger);
+            MDC.remove("logFileName");
+            Log4jUtil.stopFileAppender("thread-1", logger);
         }).start();
 
+        // add appender
         Log4jUtil.addFileAppender("main", logger);
         MDC.put("logFileName", "main");
 
+        // start log
         logger.trace("main: Hello World!");
         logger.debug("main: How are you today?");
         logger.info("main: I am fine.");
         logger.warn("main: I love programming.");
         logger.error("main: I am programming.");
 
+        // remove appender
         MDC.remove("logFileName");
         Log4jUtil.stopFileAppender("main", logger);
     }
--- src/main/java/jeff/demo/Log4jUtil.java
+++ src/main/java/jeff/demo/Log4jUtil.java
@@ -8,40 +8,37 @@ import org.apache.log4j.PatternLayout;
 
 public class Log4jUtil {
 
-    private final static String pattern = "%d{HH:mm:ss.SSS} [%t] %p %c{2}: %m%n";
+    private final static String PATTERN = "%d{HH:mm:ss.SSS} [%t] %p %c{2}: %m%n";
     public final static String DISCRIMINATOR_KEY = "logFileName";
 
     public synchronized static boolean addFileAppender(String logFileName, Logger logger) {
 
         if (logger.getAppender(logFileName) == null) {
-
             try {
-
+                // create appender
                 String logfile = "log/" + logFileName + ".log";
-                FileAppender fileAppender = new FileAppender(
-                        new PatternLayout(pattern), logfile,true);
+                FileAppender fileAppender = new FileAppender(new PatternLayout(PATTERN), logfile,true);
                 fileAppender.setName(logFileName);
                 fileAppender.addFilter(new MDCFilter(logFileName));
                 fileAppender.setImmediateFlush(true);
 
+                // add appender
                 logger.addAppender(fileAppender);
-
             } catch (Exception e) {
-                logger.warn("[ifx] Failed to add appender to log4j logger", e);
+                logger.warn("Failed to add appender to log4j logger", e);
                 return false;
             }
         }
         return true;
-
     }
 
     public static void stopFileAppender(String logFileName, Logger logger) {
 
         Appender appender = logger.getAppender(logFileName);
         if (appender != null) {
+            // close appender
             appender.close();
         }
     }
-
 }
 
